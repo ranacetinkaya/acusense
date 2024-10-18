ca0b3647636c8507e7aff5db0319d86f606e564e
Rana Çetinkaya <129221803+ranacetinkaya@users.noreply.github.com>
2024-10-18 14:06:03 +0300

    Resolved merge conflicts

--- src/App.js
+++ src/App.js
@@ -1,49 +1,28 @@
-
-import React, {  useState } from 'react';
-import axios from 'axios';
 import './App.css';
-import { upload } from '@testing-library/user-event/dist/upload';
+
+const PDF_FILE_URL = 'http://localhost::3000//CMPE 491 - Project Proposal.pdf';
+
 function App() {
-  const [file, setFile] = useState(null);
-  const [progress, setProgress] = useState({started: false,pc:0});
-  const [msg, setMsg] = useState(null);
- 
+  const downloadFileAtUrl = (url) => {
+    fetch(url)
+      .then(response => response.blob())
+      .then(blob => {
+        const blobURL = window.URL.createObjectURL(blob);
+        const fileName = url.split('/').pop();
+        const aTag = document.createElement('a');
+        aTag.href = blobURL;
+        aTag.setAttribute('download', fileName); 
+        document.body.appendChild(aTag);
+        aTag.click();
+        aTag.remove();
+        window.URL.revokeObjectURL(blobURL);
+      })
+      .catch(error => console.error('Error downloading file:', error));
+  };
 
-
-function handleUpload(){
-  if(!file){
-    console.log("No file selected")
-    return
-  }
-const fd = new FormData();
-fd.append('file',file);
-setMsg("Uploading...")
-setProgress(prevState =>{
-  return{...prevState,started:true}
-})
-axios.post('http://httpbin.org/post',fd,{
-onUploadProgress: (progressEvent) => {setProgress(prevState => {
-  return {...prevState, pc:progressEvent.progress*100}
-})},
-headers: {"Custom-Header" : "value",
-  }
-})
-.then(res =>  {
-  setMsg("upload successful")
-  console.log(res.data)
-})
-.catch(err =>{
-  setMsg("upload failed")
-  console.error(err)} );
-}
   return (
     <div className='App'>
-      <h1>File Upload </h1>
-    <input onChange={ (e) => {setFile(e.target.files[0]) } } type ="file"/>
-    <button>Upload</button>
-    <button onClick={handleUpload} > Upload</button>
-   {progress.started && <progress max="100" value ={progress.pc}></progress>}
-   {msg && <span>{msg}</span>}
+      <button onClick={() => downloadFileAtUrl(PDF_FILE_URL)}>Project Proposal</button>
     </div>
   );
 }
